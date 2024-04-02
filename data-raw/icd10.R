6d40e47ef1f8108bfc0fc03442cfd54861c28c4b
Andrew Bruce <andrewbruce.himni@gmail.com>
2024-04-01 21:22:47 -0400

    added links to current ms-drg version

--- data-raw/icd10.R
+++ data-raw/icd10.R
@@ -2,14 +2,18 @@ library(tidyverse)
 library(rvest)
 library(icd10us)
 
-# base <- "https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Publications/ICD10CM/2024"
-#
-# x <- session(base) |>
-#   session_follow_link("2024") |>
-#   html_elements("a") |>
-#   html_attr("href")
-#
-# x[2:8]
+
+download_icd10 <- function() {
+
+  base <- "https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Publications/ICD10CM/"
+  x <- rvest::session(base) |>
+    rvest::session_follow_link("2024") |>
+    rvest::html_elements("a") |>
+    rvest::html_attr("href")
+
+  x[2:8]
+
+}
 
 ## Load 2024 version
 icdcodes <- "https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Publications/ICD10CM/2024/icd10cm-CodesDescriptions-2024.zip"
--- data-raw/msdrg.R
+++ data-raw/msdrg.R
@@ -1,5 +1,26 @@
 library(rvest)
 
+# https://www.cms.gov/medicare/payment/prospective-payment-systems/acute-inpatient-pps/ms-drg-classifications-and-software
+
+# FY 2024 – Version 41.1 (Effective April 1, 2024 through September 30, 2024)
+
+# The ICD-10 Definitions of Medicare Code Edits file contains the following: A
+# description of each coding edit with the corresponding code lists as well as
+# all the edits and the code lists effective for FY 2024. Zip file contains a
+# PDF and text file that is 508 compliant.
+
+drg_v41_code_edits <- "https://www.cms.gov/files/zip/definition-medicare-code-edits-v411.zip"
+
+
+# A zip file with the ICD-10 MS DRG Definitions Manual (Text Version) contains
+# the complete documentation of the ICD-10 MS-DRG Grouper logic. - Updated
+# 03/12/2024
+
+drg_v41_manual <- "https://www.cms.gov/files/zip/icd-10-ms-drg-definitions-manual-files-v411.zip"
+
+
+
+# Version 36
 ms_drg_v36 <- read_html("https://www.hipaaspace.com/medical.coding.library/drgs/") |>
   html_element(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "table-striped", " " ))]') |>
   html_table(na.strings = c("N/A", "N/S"), convert = FALSE)
